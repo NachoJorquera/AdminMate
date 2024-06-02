06fd24dffce6862b9806e8f32805a368bbd4de4b
Ignacio Jorquera <ijorquera@alumnos.uai.cl>
2024-06-02 19:00:28 -0400

    Organización de archivos del repositorio

--- loginFrontEnd/src/App.jsx
+++ loginFrontEnd/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import  {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './Login'
-import Register from './Register'
-import Home from './Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
 
 function App() {
   return (
